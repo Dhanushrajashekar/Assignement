55ad8e1d03f240f2bb30993b4adf68e90043f136
Dhanush Rajashekar <70057181+Dhanushrajashekar@users.noreply.github.com>
2022-08-24 00:07:10 +0530

    Add files via upload

---
+++ avg.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinAndMax
+{
+    public class avg
+    {
+        public static void Main()
+        {
+            double sum = 0, avg = 0;
+            double[] numbers = { 10, 20, 50, 40 };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            avg = sum / numbers.Length;
+            Console.WriteLine($"The Sum is :{sum}");
+            Console.WriteLine($"The Average is :{avg}");
+
+           
+        }
+    }
+}
